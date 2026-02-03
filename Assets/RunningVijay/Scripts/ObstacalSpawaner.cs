5b62a37bc8200846ebb544903f1f581af348ae0e
Alok Dubey <alok.dubey@seamedu.com>
2026-02-03 14:11:15 +0530

    Spawn Obstacal

--- Assets/RunningVijay/Scripts/ObstacalSpawaner.cs
+++ Assets/RunningVijay/Scripts/ObstacalSpawaner.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class ObstacalSpawaner : MonoBehaviour
 {
+    //public GameObject enamyPrefabs;
+    public Transform spawnerPoint;
+    public GameObject[] enamyPrefabs;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Made by Unity
     void Start()
     {
-        
+        SpawnObstacals();
     }
 
     // Update is called once per frame
+    //Made by Unity
     void Update()
     {
-        
+    
+    }
+
+
+    //Made by User
+    public void SpawnObstacals()
+    {
+        // Spawn single boject
+        //Instantiate(enamyPrefabs , new Vector3(spawnerPoint.position.x, spawnerPoint.position.y, spawnerPoint.position.z), Quaternion.identity);
+
+        // Spawn Random Objects
+
+        int num = Random.Range(0, enamyPrefabs.Length);
+        Instantiate(enamyPrefabs[num], new Vector3(spawnerPoint.position.x, spawnerPoint.position.y, spawnerPoint.position.z), Quaternion.identity);
     }
 }
